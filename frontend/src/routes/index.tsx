0558cd584e97cd622799c3689e87f345605832ce
Hadi Chebbo <chebbohadi5@gmail.com>
2026-04-07 19:13:36 +0300

    implemented role middleware and protected routes (Backend + Frontend) (#6)

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '../pages/auth/LoginPage';
-import DashboardPage from '../pages/DashboardPage';
+import DashboardPage from '../pages/super-admin/DashboardPage';
 import CentersPage from '../pages/super-admin/centersPage';
+import ForbiddenPage from '../pages/ForbiddenPage';
 import ProtectedRoute from './ProtectedRoute';
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/forbidden" element={<ForbiddenPage />} />
       <Route
         path="/dashboard"
         element={
@@ -22,7 +24,7 @@ const AppRoutes: React.FC = () => {
       <Route
         path = "super-admin/centers"
         element = {
-          <ProtectedRoute>
+          <ProtectedRoute allowedRoles={["super-admin"]}>
             <CentersPage />
           </ProtectedRoute>
         }
